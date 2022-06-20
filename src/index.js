eb36e36827e1a91fa37c548db1931febaac197d6
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-06-20 14:38:26 +0300

    add fetchCountries.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+const refs = {
+    input: document.querySelector('#search-box'),
+    countryList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info'),
+}
+
+let name = '';
+const onSearch = (event) => {
+    const { value } = event.target;
+    event.preventDefault();
+    
+    if (!value) return;
+    // console.log(value);
+    name = value.trim();
+    // console.log(name);
+    fetchCountries({ name });
+        
+    
+}
+
+refs.input.addEventListener('input', _.debounce(onSearch, DEBOUNCE_DELAY));
