1650afbb22e5744c47d15a32fe9cc21b8376033f
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-06-22 23:19:12 +0300

    update

--- src/index.js
+++ src/index.js
@@ -12,6 +12,9 @@ let name = '';
 const onSearch = (event) => {
     const { value } = event.target;
     event.preventDefault();
+    refs.countryList.innerHTML = "";
+    refs.countryInfo.innerHTML = "";
+    refs.countryList.classList.remove("big");
     
     if (!value) return;
     // console.log(value);
@@ -31,15 +34,45 @@ const onSearch = (event) => {
                      name: { official },
                  
                  } = country;
-             console.log(svg, official)});
-             
+            //  console.log(svg, official);
+            refs.countryList.insertAdjacentHTML("beforeend", renderCountriesList(svg, official));
+            });
          }
          if (countries.length === 1) {
-             countries.map(country => console.log(country.name.official, country.capital, country.population, country.flags.svg, country.languages));
+             countries.map((country) => {
+                const {
+                    flags: { svg },
+                    name: { official },
+                    capital,
+                    population,
+                    languages
+                
+                } = country;
+                console.log(official, capital, population, svg, languages);
+                refs.countryList.innerHTML = renderCountriesList(svg, official);
+                refs.countryList.classList.add("big");
+                refs.countryInfo.innerHTML = renderCountriesInfo(capital, population, languages);
+            });
          }
      });
-        
+}
+
+const renderCountriesList = (svg, official) => {
+    return `<li>
+    <div class="country-name">
+      <img src="${svg}" alt="flag" width="100">
+      <h2 class="country-title">${official}</h2>
+    </div>
+  </li>`;
+ };
+
+ function renderCountriesInfo(capital, population, languages )  {
+    const languagesName = Object.values(languages).join(', ');
+    console.log(languagesName);
     
+    return `<p class="country-text"><span>Capital:</span>${capital}</p>
+            <p class="country-text"><span>Population:</span>${population}</p>
+            <p class="country-text"><span>Languages:</span>${languagesName}</p>`
 }
 
 refs.input.addEventListener('input', _.debounce(onSearch, DEBOUNCE_DELAY));
